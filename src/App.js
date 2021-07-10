398d248bd347b86e2b886234e0bd3ea3dfb16fe0
Raunak Singh <Raunak.singh.inventor@gmail.com>
2021-07-10 11:29:09 -0400

    reverted before commit, googl;e translate shows on production build

--- src/App.js
+++ src/App.js
@@ -236,15 +236,11 @@ function App() {
           {isDarkMode ? (
             <>
               <h4 style={{ color: "white" }}>
-                RELOAD the page to select different language
-              </h4>
-              <h4 style={{ color: "white" }}>
                 Made with ❤️ in React by Raunak Singh
               </h4>
             </>
           ) : (
             <>
-              <h4>RELOAD the page to select different language</h4>
               <h4>Made with ❤️ in React by Raunak Singh</h4>
             </>
           )}
